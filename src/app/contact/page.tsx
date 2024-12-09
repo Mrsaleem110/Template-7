484022aad448d4428e82ac4c4b5b6a1802c2c8c4
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-12-09 11:36:18 +0500

    add

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,149 +1,118 @@
-"use client"
-import '@fontsource/poppins'; 
-import { TiLocation } from 'react-icons/ti';
-import { FaPhone } from "react-icons/fa6";
-import { IoTimeSharp } from "react-icons/io5";
+import Image from "next/image";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
+import { TbPhoneFilled } from "react-icons/tb";
+import { TbClockFilled } from "react-icons/tb";
 
 
-export default function Contact() {
-    return (
-      <main className='font-poppins'>
-         
-    <div className="h-screen flex ">
-      <div
-        className="h-[30%]  w-full bg-cover bg-center"
-        style={{ backgroundImage: "url('/bg.jpg')" }}
-      >
-        </div >
-        </div>
-        <div className="  flex items-center justify-center ">
-    <h1 className="text-4xl  font-semibold mt-[1080px] text-black absolute top-72 font-poppins ">
-        Get In Touch With Us
-    </h1>
-    <p className="text-lg mt-[1080px] text-gray-400 font-normal font-poppins absolute top-[340px] max-w-3xl text-center">
-    For More Information About Our Product & Services. Please Feel Free To Drop Us
-     An Email Our Staff Always Be There To Help You Out. Do Not Hesitate!
-    </p>
-        </div>
 
-        <div className="flex flex-col md:flex-row items-center md:space-x-12">
-          {/* Contact Info Section */}
-          <div className="flex-1 mx-60 -mt-64 ">
-            
-            <div className="mb-4">
-              <TiLocation className="inline-block text-3xl  mr-2 -mx-11" />
-              <div>
-      <h2 className=" font-bold text-gray-900 text-2xl">Address</h2>
-      <p className=" font-medium text-black w-48 mt-1">236 5th Avenue, New York NY10000, United States</p>
-    </div>
-            </div>
-            <div className="mb-4 mt-5">
-              <FaPhone className="inline-block text-3xl -mx-11 mr-2" />
-              <div>
-      <h2 className=" font-bold text-gray-900 text-2xl">Phone</h2>
-      <p className=" text-black mt-1 font-medium">Mobile: +(84) 546-6789</p>
-      <p className=" text-black mt-1 font-medium">Hotline: +(84) 456-6789</p>
-    </div>
-            <div className="mb-4 mt-5">
-              <IoTimeSharp  className="inline-block  -mx-11 text-3xl mr-2" />
-              <div>
-      <h2 className=" font-bold text-gray-900 text-2xl">Working Time</h2>
-      <p className=" text-black mt-1 font-medium">Monday-Friday: 9:00-
-        22:00
-      </p>
-      <p className=" text-black mt-1 font-medium">Saturday-Sunday: 9:00-21:00</p>
-    </div>
-            </div>
-          </div></div ></ div>
-
-          <div className="flex-1 w-[600px] float-right mr-40 -mt-[430px] ">
-            <form
-              className="space-y-4"
-              
-              
-            >
-              <div>
-                <label htmlFor="name" className="block mb-2">
-                  Your Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  
-                  
-                  className="w-full p-2 rounded bg-white border border-gray-600 focus:outline-none "
-                  placeholder="Enter Your Name"
-                  required
-                />
-              </div>
-
-              <div>
-                <label htmlFor="email" className="block mb-2">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  
-                  className="w-full p-2 rounded bg-white border border-gray-600 focus:outline-none focus:border-customBlue"
-                  placeholder="Enter Your Email"
-                  required
-                />
-              </div>
-
-              <div>
-                <label htmlFor="subject" className="block mb-2">
-                  Subject
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  rows={2}
-                  
-                  className="w-full p-2 rounded bg-white border border-gray-600 focus:outline-none focus:border-customBlue"
-                  placeholder="Enter Your Message"
-                  required
-                />
-              </div>
-
-              <div>
-                <label htmlFor="message" className="block mb-2">
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  rows={3}
-                  
-                  className="w-full p-2 rounded bg-white border border-gray-600 focus:outline-none focus:border-customBlue"
-                  placeholder="Enter Your Message"
-                  required
-                />
-              </div>
-   <div className='py-5'>
-              <button 
-                type="submit"
-                
-                className=" text-white h-11 bg-yellow-700 w-36 "
-              >Submit
-                
-              </button></div >
-            </form>
+export default function () {
+  return (
+    <>
+      <div className="relative">
+        <Image src="/Pictures/shophero.png " alt="Picture of the author" />
+        <div className="absolute top-[50%] left-[50%] flex justify-center flex-col items-center ">
+          <h3 className="font-bold text-[1.5rem]">Contact</h3>
+          <div className="flex items-center">
+            <h3>Home</h3>
+            <IoIosArrowForward />
+            <h3>contact</h3>
           </div>
+        </div>
+        {/* <div> */}
+        {/* </div> */}
+      </div>
+      <div className="text-center w-15 mt-3">
+        <h2 className="font-extrabold text-[2rem]">Get In Touch With Us</h2>
+        <p>
+          For More Information About Our Product & Services. Please Feel Free To Drop Us<br /> An Email. Our Staff Always Be There To Help You Out. Do Not
+          Hesitate!
+        </p>
+      </div>
+      <div className="flex justify-evenly leading-8  ">
+        <div className="w-[14rem] mt-6  ">
+        <div className="flex  ">
+                    <div>
 
-          <div className="h-screen flex mt-36 ">
-      <div
-        className="h-[20%]  w-full bg-cover bg-center"
-        style={{ backgroundImage: "url('/cool.jpg')" }}
-      >
-        </div >
+
+                <FaLocationDot/> 
+                    </div>
+                    <div>
+
+            <h2 className="font-bold">Address</h2>
+            <p>236 5th SE Avenue, New York NY10000, United States</p>
+                    </div>
+
+        </div>
+        <div className="flex">
+            <div>
+
+            <TbPhoneFilled/>
+            </div>
+            <div>
+
+            <h2 className="font-bold">Phone</h2>
+                <p>Mobile: +(84) 546-6789 </p>
+                <p>Hotline: +(84) 456-6789</p>
+            </div>
+        </div>
+        <div className="flex">
+            <div>
+
+            < TbClockFilled/>
+            </div>
+            <div>
+
+            <h2 className="font-bold">Working Time</h2>
+            <p>Monday-Friday: 9:00 - 22:00 </p>
+            <p>Saturday-Sunday: 9:00 - 21:00</p>
+            </div>
+
+        </div>
+
+        </div>
+
+        <div className="mt-6 ">
+            <form className="flex  flex-col ... " action="" >
+                <label htmlFor="">Name</label>
+                <input type="text" className="border-gray-200 border-2 p-2 rounded-md w-[17rem]" placeholder="name" />
+                <label htmlFor="">Email</label>
+                <input type="text" className="border-gray-200 border-2 p-2 rounded-md w-[17rem]" placeholder="email" />
+                <label htmlFor="">Subject</label>
+                <input type="text" className="border-gray-200 border-2 p-2 rounded-md w-[17rem]" placeholder="optional" />
+                <label htmlFor="">Message</label>
+                <input type="text" className="border-gray-200 border-2 p-2 rounded-md w-[17rem]" placeholder="Hi! i’d like to ask about" />
+                <button className="self-start bg-[#B88E2F] text-white mt-6 p-3 w-[7rem] rounded-md">Submit</button>
+            </form>
         </div>
         
+      </div>
+      <div className=" mt-5 mb-5 flex justify-evenly items-center p-2 w-[100%] h-[8rem] bg-[#F9F1E7]">
         
-  
-
-      </main>
-    )
-  }
+        < div className="">
+    
+        <Image src="/Group.png" width={40} height={40} alt="Picture of the author" />
+        
+          <h4 className="font-bold">High Quality</h4>
+          <p>crafted from top materials</p>
+        </div>
+        <div>
+        <Image src="/warr.png" width={40} height={40} alt="Picture of the author" />
+          <h4 className="font-bold">Warranty Protection</h4>
+          <p>Over 2 years</p>
+        </div>
+        <div>
+        <Image src="/vector.png" width={40} height={40} alt="Picture of the author" />
+          <h4 className="font-bold">Free Shipping</h4>
+          <p>Order over 150 $</p>
+        </div>
+        <div>
+        <Image src="/customer.png" width={40} height={40} alt="Picture of the author" />
+          <h4 className="font-bold">24 / 7 Support</h4>
+          <p>Dedicated support</p>
+        </div>
+      </div>
+    
+    </>
+  );
+}
